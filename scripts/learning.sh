ac7491e21521492ed3f2685431318251ab583b09
Shivang Gupta <creatorvision03@gmail.com>
2020-04-23 03:39:39 +0400

    Create learning.sh

---
+++ scripts/learning.sh
@@ -0,0 +1,16 @@
+# data science
+open https://learn.datacamp.com/career-tracks/data-scientist-with-python
+open http://scpd.stanford.edu/home
+open https://www.coursera.org/
+open https://courses.edx.org/dashboard
+open https://developers.google.com/machine-learning/crash-course/framing/video-lecture
+open https://www.educba.com/my-courses/bundle/
+open https://datahack.analyticsvidhya.com/contest/all/
+open https://www.kaggle.com/learn/intro-to-machine-learning
+open https://data.world/search?q=covid19
+open https://python.quantecon.org/
+open https://www.hackerearth.com/practice/machine-learning/prerequisites-of-machine-learning/basic-probability-models-and-rules/tutorial/
+open https://openai.com/
+open https://beingdatum.com/course/guide-on-classical-machine-learning/
+open http://r-statistics.co/R-Tutorial.html
+open https://www.statsmodels.org/stable/generated/statsmodels.regression.linear_model.WLS.html
