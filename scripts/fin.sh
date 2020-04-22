3b3886aeb5948be695933fc5ea16408504da6b7b
Shivang Gupta <creatorvision03@gmail.com>
2020-04-22 18:55:22 +0400

    scripts

---
+++ scripts/fin.sh
@@ -0,0 +1,7 @@
+open https://docs.google.com/spreadsheets/d/1f_VDqiipZUT0d_y1Snrk7icRVjuuuq5uNPo83MYk3rw/edit#gid=1271640285
+open https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-tax-relief-96/MBS291
+open https://arch.icicipruamc.com/
+open https://www.adityabirlacapital.com/my-dashboard
+open https://online.tatamutualfund.com/transact?SID=f9a4344c3f1bae1fdef3dc3f9ea06363bc7aa5ec8c6a2050282176221cd0437b#!
+open https://transact.miraeassetmf.co.in/investor/General/Login.aspx#openModal
+open https://kite.zerodha.com/
---
+++ scripts/links.sh
@@ -0,0 +1,20 @@
+open https://www.qwiklabs.com/home?locale=en
+open https://classroom.udacity.com/me
+open https://lagunita.stanford.edu/courses/HumanitiesSciences/StatLearning/Winter2016/course/
+open https://courses.edx.org/dashboard
+open https://ai.google/education/
+open https://data.gov.in/node/3959481
+open https://toolbox.google.com/datasetsearch
+open https://www.hackerrank.com/domains/algorithms?filters%5Bstatus%5D%5B%5D=unsolved&badge_type=problem-solving
+open https://www.hackerearth.com/practice/machine-learning/prerequisites-of-machine-learning/basic-probability-models-and-rules/tutorial/
+open https://github.com/GokuMohandas/practicalAI
+open https://github.com/FavioVazquez/ds-cheatsheets
+open https://github.com/anhquan0412/basic_model_scratch
+open https://machinelearningmastery.com/statistical-hypothesis-tests/
+open https://towardsdatascience.com/whats-new-in-deep-learning-research-creating-adaptable-meta-learning-models-ccf25b6c7dc4
+open https://ocw.mit.edu/courses/mathematics/18-05-introduction-to-probability-and-statistics-spring-2014/syllabus/
+open https://www.youtube.com/watch?v=xoA5v9AO7S0&list=PLLssT5z_DsK9JDLcT8T62VtzwyW9LNepV&index=1
+
+# blogs
+
+open https://www.bigcommerce.com/blog/value-based-ecommerce-pricing-strategy/#undefined
